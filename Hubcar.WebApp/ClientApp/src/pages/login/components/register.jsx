a82d337efbebf839cefa720d053caf9105d77388
Alessandra <alessandra.paulafrancisco@gmail.com>
2020-08-17 18:53:39 -0300

    Finalizando a tela de cadastro

--- Hubcar.WebApp/ClientApp/src/pages/login/components/register.jsx
+++ Hubcar.WebApp/ClientApp/src/pages/login/components/register.jsx
@@ -1,10 +1,114 @@
-import React from 'react';
-import { Button, Form, FormGroup, Input } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, FormGroup, Input, Col, Row } from 'reactstrap';
+import  InputMask from 'react-input-mask';
+import './login.css';
+import axios from 'axios';
 
 const Register = () => {
+  let [ufsIBGE, setUfsIBGE] = useState([]);
+  let [cidadesIBGE, setCidadesIBGE] = useState([]);
+  let [uf, setUf] = useState();
+  let [values, setValues] = useState({});
+  let [cpf, setCpf] = useState({});
+
+  useEffect(() => {
+    axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+      .then(response => {
+      const ufInitials = response.data.map(uf => uf.sigla);
+      setUfsIBGE(ufInitials);
+    });
+  }, []);
+  
+  useEffect(() => {
+    axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+    .then(response => {
+      const cidades = response.data.map(cidade => cidade.nome);
+      setCidadesIBGE(cidades);
+    });  
+  }, [uf]);
+
+  function handleChange(event){
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function handleChangeUf(event){
+    let uf = event.target.value;
+    setValues({
+      ...values,
+      uf: uf
+    });
+    setUf(uf);
+  }
+
+  function handleChangeCity(event){
+    let cidade = event.target.value;
+    setValues({
+      ...values,
+      cidade: cidade
+    });
+  }
+
+  function handleChangeCpf(event){
+    let cpf = event.target.value;
+    let newState = {
+      mask: '999.999.999-99',
+      value: cpf
+    };
+    setCpf(newState);
+    setValues({
+      ...values,
+      cpf: cpf
+    })
+  }
+
+  function handleSave(){
+
+  }
+
+
   return (
     <Form>
-      
+      <FormGroup>
+        <Input type="text" name="nome" onChange={handleChange} placeholder="Nome completo" />
+      </FormGroup>
+      <FormGroup>
+        <InputMask 
+          className="input" 
+          nome="cpf" 
+          placeholder="CPF" 
+          {...cpf}
+          onChange={handleChangeCpf}/>
+      </FormGroup>
+      <FormGroup>
+        <Input type="email" name="email" id="exampleEmail" onChange={handleChange} placeholder="Email." />
+      </FormGroup>
+      <FormGroup>
+        <Input type="password" name="senha" id="examplePassword" onChange={handleChange} placeholder="Senha" />
+      </FormGroup>
+      <FormGroup>
+        <Row>
+          <Col md={4}>
+            <Input type="select" name="select" id="exampleSelect" onChange={handleChangeUf}>
+              <option value="0">UF</option>
+              {ufsIBGE.map(estado => (
+                <option key={estado} value={estado}>{estado}</option>
+              ))}
+            </Input>
+          </Col>
+          <Col md={8}>
+            <Input type="select" name="select" id="exampleSelect" onChange={handleChangeCity}>
+              <option value="0">Cidade</option>
+              {cidadesIBGE.map(city => (
+                <option key={city} value={city}>{city}</option>
+              ))}
+            </Input>
+          </Col>
+        </Row>
+      </FormGroup>
+      <Button className="button" onClick={handleSave}>Cadastro</Button>
     </Form>
   );
 }
--- Hubcar.WebApp/ClientApp/src/pages/login/index.jsx
+++ Hubcar.WebApp/ClientApp/src/pages/login/index.jsx
@@ -5,14 +5,14 @@ import Register from './components/register';
 import { Card } from 'reactstrap';
 
 const Login = () => {
-  const [ register, setRegister] = useState(false);
+  const [ register, setRegister] = useState(0);
 
   useEffect(() =>{
     
   }, []);
 
   function handleChangeRegister(){
-    setRegister(true);
+    setRegister(register + 1);
   }
 
   return(
@@ -20,12 +20,14 @@ const Login = () => {
       <div className="login">
         <Card className="card">
           {
-            !register ?
+            register % 2 == 0?
             <FormLogin/>
             :
-            <Register handle/>
+            <Register/>
           }
-          <a style={{marginTop: 15}} onClick={handleChangeRegister} href="">{register ? "Login" : "Cadastre-se"}</a>
+          <a style={{marginTop: 15}} className="link" onClick={handleChangeRegister}>
+            {register % 2 != 0? "Login" : "Cadastre-se"}
+          </a>
         </Card>
       </div>
     </div>

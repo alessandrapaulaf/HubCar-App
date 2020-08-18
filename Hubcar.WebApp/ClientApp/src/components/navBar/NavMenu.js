72b52f236dc5ef949351bc1ac8b525ec5ad2c0b9
Alessandra <alessandra.paulafrancisco@gmail.com>
2020-08-18 17:16:57 -0300

    Estilizando a página Home

--- Hubcar.WebApp/ClientApp/src/components/navBar/NavMenu.js
+++ Hubcar.WebApp/ClientApp/src/components/navBar/NavMenu.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Logo from './../../assets/logoHigh.png';
+import { BsFillPlusSquareFill, BsFillPersonFill } from 'react-icons/bs';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -26,18 +28,15 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Hubcar.WebApp</NavbarBrand>
+            <NavbarBrand tag={Link} to="/"><img src={Logo} className="logo"/></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="icon" to="/create-carro"><BsFillPersonFill/></NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="icon" to="/counter"><BsFillPlusSquareFill/></NavLink>
                 </NavItem>
               </ul>
             </Collapse>
--- Hubcar.WebApp/ClientApp/src/pages/home-page/index.jsx
+++ Hubcar.WebApp/ClientApp/src/pages/home-page/index.jsx
@@ -1,13 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import { Card, Button, Form, FormGroup, Input, Label, Col, Row } from 'reactstrap';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import './home.css';
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import { red } from '@material-ui/core/colors';
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
+const defaultMaterialTheme = createMuiTheme({
+  palette: {
+    primary: red,
+  },
+});
 
+const Home = () => {
+  let [map, setMap] = useState({});
+
+  useEffect(() => {
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlcGZyYW5jaXMiLCJhIjoiY2tlMGJnZ3FzMnFqNDJ1bnVrY2l0cmNjdCJ9.QmrWXa9N5sFWk0DOxkO0Aw';
+    setMap(new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    }));
+  }, []);
+
+  return (
+    <div className="content">
+      <div className="form">
+        <Card style={{padding: 0}} className="card">
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Onde deseja retirar seu carro?</Label>
+              <Input type="text"  placeholder="Digite seu endereço" />
+            </FormGroup>
+            <FormGroup>
+              <ThemeProvider theme={defaultMaterialTheme}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Date picker inline"
+                    // value={selectedDate}
+                    // onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                </MuiPickersUtilsProvider>
+              </ThemeProvider>
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+            </FormGroup>
+            <Button></Button>
+          </Form>
+        </Card>
       </div>
-    );
-  }
+      <div id='map' style={{width: 600, height: 450}}></div>
+    </div>
+  );
 }
 
 export default Home;

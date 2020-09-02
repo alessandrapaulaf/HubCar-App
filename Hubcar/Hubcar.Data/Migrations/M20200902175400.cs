1e26a916cecf00bb91cf386035ceff4aed9ede70
Alessandra <alessandra.paulafrancisco@gmail.com>
2020-09-02 18:27:11 -0300

    Iniciando as migrations do EF

---
+++ Hubcar/Hubcar.Data/Migrations/M20200902175400.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Hubcar.Data.Migrations
+{
+    class M20200902175400 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(nullable: false),
+                    Senha = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Cpf = table.Column<string>(nullable: false, maxLength: 11),
+                    Cidade = table.Column<string>(nullable: false),
+                    UF = table.Column<string>(nullable: false, maxLength: 2),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", p => p.Id);
+                }
+            );
+
+            migrationBuilder.CreateTable(
+                name: "Carro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            base.Down(migrationBuilder);
+        }
+    }
+}
